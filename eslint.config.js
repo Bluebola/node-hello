f011b9d321ab18c4bd13857be8c82130684f82f4
Timothy <tlzw2003@gmail.com>
2025-09-22 05:17:13 +0800

    Added PR validation CICD pipeline

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        exports: "writable",
+        module: "writable",
+        require: "readonly",
+        global: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
